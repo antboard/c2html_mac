ab0369738976f0cfc55d07219886c0bbd28fb766
icecut <icecut@qq.com>
2014-10-20 19:35:02 +0800

    Initial Commit

---
+++ c2html_mac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c2html_mac
+//
+//  Created by 姜亦春 on 14/10/20.
+//  Copyright (c) 2014年 antboard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
